de0045dc79ff18fccd9c53a8caca373329518546
Luis-coelho30 <luis.coelho.761@gmail.com>
2025-02-18 16:49:50 -0300

    Adicionando os verbos HTTP

---
+++ my-first-web-api/src/main/java/web/api/repository/UsuarioRepository.java
@@ -0,0 +1,35 @@
+package web.api.repository;
+
+import org.springframework.stereotype.Repository;
+import web.api.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+
+    public void save(Usuario usuario){
+        System.out.println("SAVE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void update(Usuario usuario){
+        System.out.println("UPDATE - Recebendo o usuário na camada de repositório");
+        System.out.println(usuario);
+    }
+    public void remove(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+    public List<Usuario> listAll(){
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("luis","password"));
+        usuarios.add(new Usuario("joao","senha123"));
+        return usuarios;
+    }
+    public Usuario findByUsername(String username){
+        System.out.println(String.format("GET/Username - Recebendo o login: %s para localizar um usuário", username));
+        return new Usuario("luis","password");
+    }
+
+}
