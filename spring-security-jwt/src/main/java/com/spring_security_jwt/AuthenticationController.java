f991fc30671c59f8030c9ebb86adcf04353772fb
Luís Coelho <luis.coelho.761@gmail.com>
2025-04-04 12:42:02 -0300

    Adicionando projeto com Spring Security JWT

---
+++ spring-security-jwt/src/main/java/com/spring_security_jwt/AuthenticationController.java
@@ -0,0 +1,19 @@
+package com.spring_security_jwt;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("authenticate")
+    public String authenticate(Authentication authentication) { //Injetado no momento do login
+        return authenticationService.authenticate(authentication);
+    }
+}
